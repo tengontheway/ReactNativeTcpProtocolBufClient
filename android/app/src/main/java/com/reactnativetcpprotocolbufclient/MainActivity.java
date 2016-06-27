9a206c3f5bfc14bd2db7b8f5813aa5249fe8571a
Evil.T <teng_ontheway@163.com>
2016-06-27 20:52:20 +0800

    增加了依赖库，client能正常编译并发送消息

--- android/app/src/main/java/com/reactnativetcpprotocolbufclient/MainActivity.java
+++ android/app/src/main/java/com/reactnativetcpprotocolbufclient/MainActivity.java
@@ -1,6 +1,7 @@
 package com.reactnativetcpprotocolbufclient;
 
 import com.facebook.react.ReactActivity;
+import com.peel.react.TcpSocketsModule;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
@@ -34,7 +35,8 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
-            new MainReactPackage()
+            new MainReactPackage(),
+            new TcpSocketsModule()
         );
     }
 }
--- index.ios.js
+++ index.ios.js
@@ -12,19 +12,55 @@ import {
   View
 } from 'react-native';
 
+import LoginMsg from './protobuf/test_pb';
+var ByteBuffer = require('byte-buffer');
+
+global.Buffer = global.Buffer || require('buffer').Buffer;
+var net = require('react-native-tcp');
+
+
 class ReactNativeTcpProtocolBufClient extends Component {
+  constructor(props) {
+    super(props);
+
+    var client = net.createConnection({host:'192.168.0.162', port:8888}, ()=> {
+          console.log("--------create connection succeed!");
+
+          // 1.填充ProtocolBuff
+          var login_msg = new LoginMsg.Login();
+          login_msg.setSeed(5);
+
+          var data = login_msg.serializeBinary();
+
+          // 2.填充消息: MsgLength、MsgID、MsgData
+          var buff = new ByteBuffer(2 + 2 + data.length);
+          buff.writeUnsignedShort(2 + data.length);
+          buff.writeUnsignedShort(1);
+          buff.write(data);
+
+          // 3.我对JS的Uint8Array和什么的Array已经无语了，先这么转吧，能正常运行
+          // 心中一万个草泥马...
+          var buffers = new Uint8Array(buff.buffer);
+          client.write(buffers);
+
+          alert("Send buffers:" + buffers);
+          console.log("SendBuff:" + buffers);
+
+          client.on('error', function(error) {
+              console.log(">>>>>>>>>error:" + error);
+          });
+
+          client.on('data', function(data) {
+              console.log('message was received', data)
+          });
+      });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
         </Text>
       </View>
     );
